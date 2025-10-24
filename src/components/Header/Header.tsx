0d7fb3b4f1f5645f137fef4a246fa583f0200548
Kevesta-777 <userspace118@gmail.com>
2025-10-24 19:12:26 +0900

    add some changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,45 @@
 import { ChevronDownIcon } from "lucide-react";
-import React from "react";
-import { Button } from "../../../../components/ui/button";
+import { Link, useLocation } from "react-router-dom";
+import { Button } from "../ui/button";
+
+export interface HeaderProps {
+  backgroundImage?: string;
+}
 
 const navigationItems = [
-  { label: "Platform", hasDropdown: false, isActive: true },
-  { label: "Solutions", hasDropdown: true, isActive: false },
-  { label: "Enterprises", hasDropdown: true, isActive: false },
-  { label: "Developers", hasDropdown: false, isActive: false },
-  { label: "Pricing", hasDropdown: false, isActive: false },
+  { label: "Platform", hasDropdown: false, path: "/about" },
+  { label: "Solutions", hasDropdown: true, path: null },
+  { label: "Enterprises", hasDropdown: true, path: null },
+  { label: "Developers", hasDropdown: false, path: null },
+  { label: "Pricing", hasDropdown: false, path: null },
 ];
 
-export const HeaderWrapperSubsection = (): JSX.Element => {
+export const Header = ({ backgroundImage }: HeaderProps): JSX.Element => {
+  const location = useLocation();
+  
+  const headerStyle = backgroundImage 
+    ? {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }
+    : {
+      };
+  
   return (
-    <div className="flex flex-col w-full items-start gap-2.5 px-[110px] py-[17px] border-b-2 [border-bottom-style:solid] border-[#ffffff1a] bg-[linear-gradient(169deg,rgba(39,174,96,1)_0%,rgba(0,82,204,1)_100%)]">
+    <div 
+      className="flex flex-col w-full items-start gap-2.5 px-[110px] py-[17px] border-b-2 [border-bottom-style:solid] border-[#ffffff1a] bg-[linear-gradient(169deg,rgba(39,174,96,1)_0%,rgba(0,82,204,1)_100%)]"
+      style={headerStyle}
+    >
       <header className="relative w-full h-20 bg-transparent">
-        <img
-          className="absolute top-[calc(50.00%_-_19px)] left-0 w-[184px] h-[38px]"
-          alt="Group"
-          src="/logo.png"
-        />
+        <Link to="/">
+          <img
+            className="absolute top-[calc(50.00%_-_19px)] left-0 w-[184px] h-[38px] cursor-pointer"
+            alt="Group"
+            src="/logo.png"
+          />
+        </Link>
 
         <nav className="inline-flex items-center gap-16 absolute top-[29px] left-1/2 -translate-x-1/2">
           {navigationItems.map((item, index) => (
@@ -30,12 +51,21 @@ export const HeaderWrapperSubsection = (): JSX.Element => {
                   </span>
                   <ChevronDownIcon className="w-[12.59px] h-3.5 text-[#d8d8d8]" />
                 </button>
+              ) : item.path ? (
+                <Link
+                  to={item.path}
+                  className={`relative flex items-center justify-center w-fit [font-family:'DM_Sans_18pt-Regular',Helvetica] font-normal text-[17.7px] tracking-[0] leading-[18px] whitespace-nowrap bg-transparent border-none cursor-pointer ${
+                    location.pathname === item.path ? 'text-white' : 'text-[#d8d8d8]'
+                  }`}
+                >
+                  {item.label}
+                </Link>
               ) : (
                 <button className="relative flex items-center justify-center w-fit [font-family:'DM_Sans_18pt-Regular',Helvetica] font-normal text-[#d8d8d8] text-[17.7px] tracking-[0] leading-[18px] whitespace-nowrap bg-transparent border-none cursor-pointer">
                   {item.label}
                 </button>
               )}
-              {item.isActive && item.label === "Platform" && (
+              {location.pathname === item.path && (
                 <div className="absolute top-[30px] left-[-5px] w-[85px] h-1 bg-white rounded-[30px]" />
               )}
             </div>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,12 +4,12 @@ import { BackgroundSubsection } from "./sections/BackgroundSubsection";
 import { BackgroundWrapperSubsection } from "./sections/BackgroundWrapperSubsection";
 import { DivSubsection } from "./sections/DivSubsection";
 import { DivWrapperSubsection } from "./sections/DivWrapperSubsection";
-import { FooterSubsection } from "./sections/FooterSubsection";
+import { Footer } from "../../components/Footer";
 import { FormSubsection } from "./sections/FormSubsection";
 import { Frame1Subsection } from "./sections/Frame1Subsection";
 import { FrameSubsection } from "./sections/FrameSubsection";
 import { FrameWrapperSubsection } from "./sections/FrameWrapperSubsection";
-import { HeaderWrapperSubsection } from "./sections/HeaderWrapperSubsection";
+import { Header } from "../../components/Header";
 import { HeroSliderSubsection } from "./sections/HeroSliderSubsection";
 import { SectionComponentNodeSubsection } from "./sections/SectionComponentNodeSubsection";
 
@@ -46,7 +46,7 @@ export const Home = (): JSX.Element => {
         src="/icon-star-1.svg"
       />
 
-      <HeaderWrapperSubsection />
+      <Header backgroundImage = {''}/>
       <HeroSliderSubsection />
       <FrameSubsection />
       <FrameWrapperSubsection />
@@ -57,7 +57,7 @@ export const Home = (): JSX.Element => {
       <BackgroundWrapperSubsection />
       <FormSubsection />
       <SectionComponentNodeSubsection />
-      <FooterSubsection />
+      <Footer />
     </main>
   );
 };
